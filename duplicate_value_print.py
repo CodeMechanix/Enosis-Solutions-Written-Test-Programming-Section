c95f682aa89cbc1f4d2784452b5503e1d434768c
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2020-02-26 12:37:21 +0600

    Update duplicate_value_print.py

--- duplicate_value_print.py
+++ duplicate_value_print.py
@@ -1,9 +1,10 @@
 
-arr = [1, 2, 3, 4, 2, 7, 8, 8, 3];     
+arr = [1, 2, 3, 4, 2, 7, 8, 8, 3, 2];     
      
-
+repeated = [] 
 #Searches for duplicate element    
 for i in range(0, len(arr)):    
     for j in range(i+1, len(arr)):    
-        if(arr[i] == arr[j]):    
-            print(arr[j]); 
+        if arr[i] == arr[j] and arr[i] not in repeated:    
+            repeated.append(arr[i])
+print(repeated)
